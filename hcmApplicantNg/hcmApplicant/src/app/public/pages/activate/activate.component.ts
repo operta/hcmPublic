69c59df1ddb31d72ee4997999b1fb638296c86eb
Dzan Operta <dzanoperta@yahoo.com>
2020-02-12 14:04:28 +0100

    implmented email activation

--- hcmApplicantNg/hcmApplicant/src/app/public/pages/activate/activate.component.ts
+++ hcmApplicantNg/hcmApplicant/src/app/public/pages/activate/activate.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AccountService} from '../../../services/account.service';
 
 @Component({
-  selector: 'app-activate',
-  templateUrl: './activate.component.html',
-  styleUrls: ['./activate.component.css']
+    selector: 'app-activate',
+    templateUrl: './activate.component.html',
+    styleUrls: ['./activate.component.css']
 })
 export class ActivateComponent implements OnInit {
 
-  constructor() { }
+    error: string;
+    success: string;
 
-  ngOnInit() {
-  }
+    constructor(
+        private accountService: AccountService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+    }
+
+    ngOnInit() {
+        this.route.queryParams.subscribe((params) => {
+            this.accountService.activate(params['key']).subscribe(() => {
+                this.error = null;
+                this.success = 'OK';
+            }, () => {
+                this.success = null;
+                this.error = 'ERROR';
+            });
+        });
+    }
+
+    login() {
+        this.router.navigateByUrl('/login');
+    }
 
 }
--- hcmApplicantNg/hcmApplicant/src/app/services/account.service.ts
+++ hcmApplicantNg/hcmApplicant/src/app/services/account.service.ts
@@ -1,17 +1,28 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
 import {SERVER_API_URL} from '../../environments/constants';
 import {Observable} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class AccountService  {
-  constructor(private http: HttpClient) { }
+export class AccountService {
+    constructor(private http: HttpClient) {
+    }
 
-  get(): Observable<HttpResponse<Account>> {
-    return this.http.get<Account>(SERVER_API_URL + 'api/account', {observe : 'response'});
-  }
+    get(): Observable<HttpResponse<Account>> {
+        return this.http.get<Account>(SERVER_API_URL + 'api/account', {observe: 'response'});
+    }
 
-  save(account: any): Observable<HttpResponse<any>> {
-    return this.http.post(SERVER_API_URL + 'api/account', account, {observe: 'response'});
-  }
+    save(account: any): Observable<HttpResponse<any>> {
+        return this.http.post(SERVER_API_URL + 'api/account', account, {observe: 'response'});
+    }
+
+    activate(key: string): Observable<any> {
+        let options: HttpParams = new HttpParams();
+        options = options.append('key', key);
+        return this.http.get(SERVER_API_URL + 'api/activate', {
+            params: options
+        });
+    }
+
 }
+
